9d5c3751f66888221f43750cf7cbd6c038e05fd0
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-15 22:01:20 -0300

    create satrtup file to correct deployment in azure

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,10 @@ from flask import Flask
 from app.view import init_app
 from flask_cors import CORS
 
-def create_app():
+app = Flask(__name__, static_folder=None)
+init_app(app)
+CORS(app)
 
-    app = Flask(__name__, static_folder=None)
-    init_app(app)
-    CORS(app)
 
-    return app
 
 
