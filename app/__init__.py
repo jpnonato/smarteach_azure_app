31b790f60b97217aea775570d2022dcd00e9572f
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-17 12:15:23 -0300

    adjust app run in port 8000 when dploy starts

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_cors import CORS
+import logging
 
 app = Flask(__name__, static_folder=None)
 
@@ -8,6 +9,6 @@ from app.view import init_app
 init_app(app)
 CORS(app)
 
-
-
-
+if __name__ == '__main__':
+    logging.info(">>>>>>>>>>>>>>>>>>>>>  app started")
+    app.run(host="0.0.0.0", port=8000)
