7a8eb22baa9d52a7f29b11393ac47cf3dc46eae4
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-22 02:14:41 -0300

    teste123

--- app/__init__.py
+++ app/__init__.py
@@ -1,50 +1,20 @@
 
 from flask import Flask, jsonify
-# from flask_cors import CORS
-from pymongo import MongoClient
-from dotenv import load_dotenv
-# from services import get_items_data
-import os
-import urllib.parse
 
 
 app = Flask(__name__, static_folder=None)
-
-# from app.view import init_app
-
-# init_app(app)
-# CORS(app)
-
-load_dotenv()
-
-DB_USER = urllib.parse.quote_plus(os.getenv('DB_USER'))
-PASSWORD = urllib.parse.quote_plus(os.getenv('PASSWORD'))
-STR_CONNECTION = os.getenv('DB_STR_CONNECTION').format(DB_USER, PASSWORD)
-
-client = MongoClient(STR_CONNECTION)
-
-db = client['SmarTeach']
-db_collections = db.list_collection_names()
-app_collections = ['Professores', 'Alunos', 'Admins', 'Turmas']
-
-if db_collections != app_collections:
-    for collection_name in app_collections:
-        if collection_name not in db_collections:
-            db.create_collection(collection_name)
-
-teacher_collection = db.get_collection('Professores')
-student_collection = db.get_collection('Alunos')
-admin_collection = db.get_collection('Admins')
-classes_collection = db.get_collection('Turmas')
 
 
 @app.get('/admin')
 def show_admins():
 
-    data_list = [data for data in admin_collection.find({})]
-
-    for index, elt in enumerate(data_list):
-        data_list[index] = {key: elt[key] if key != '_id' else str(elt[key]) for key in elt}
+    data_list = [{
+            "1": 1000
+        },
+        {
+            "2":2000
+        }
+    ]
 
 
     return jsonify(data_list), 200
