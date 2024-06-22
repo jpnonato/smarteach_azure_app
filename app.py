563b163ef8c9f48985a397e1f4990fa56aae7e7e
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-22 12:00:39 -0300

    teste777 4.9

--- app.py
+++ app.py
@@ -1,38 +1,12 @@
 from flask import Flask,jsonify
-from pymongo import MongoClient
-import urllib.parse
 
-# from services import get_items_data
 from view import init_app
-
-# from services import get_items_data
 
 app = Flask(__name__)
 
-DB_USER = urllib.parse.quote_plus('smarTeachAdmin')
-PASSWORD = urllib.parse.quote_plus('i9vii-d@5V*4_TR')
-URL_CONNECTION = "mongodb+srv://{}:{}@cluster0.zhk40vn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-STR_CONNECTION = URL_CONNECTION.format(DB_USER, PASSWORD)
-
-client = MongoClient(STR_CONNECTION)
-
-db = client['SmarTeach']
-db_collections = db.list_collection_names()
-app_collections = ['Professores', 'Alunos', 'Admins', 'Turmas']
-
-if db_collections != app_collections:
-    for collection_name in app_collections:
-        if collection_name not in db_collections:
-            db.create_collection(collection_name)
-
-teacher_collection = db.get_collection('Professores')
-student_collection = db.get_collection('Alunos')
-admin_collection = db.get_collection('Admins')
-classes_collection = db.get_collection('Turmas')
-
-@app.get('/')
-def teste123():
-    return '<h1>---------IS WORKING now 7.4 !4--------- <h1>', 200
+# @app.get('/')
+# def teste123():
+#     return '<h1>---------IS WORKING now 7.4 !4--------- <h1>', 200
 
 # @app.get('/admin')
 # def teste777():
@@ -40,7 +14,6 @@ def teste123():
 #     return jsonify(data_list), 200
 
 init_app(app)
-
 
 if __name__ == '__main__':
     app.run()
