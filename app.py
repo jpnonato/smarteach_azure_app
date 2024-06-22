2ddee8e8bc9b005fcb56332879720bc77858b293
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-22 03:19:10 -0300

    teste 123 11

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.get('/')
 def teste123():
-    return '<h1>---------IS WORKING 7777!!!--------- <h1>', 200
+    return '<h1>---------IS WORKING now!!!--------- <h1>', 200
 
 
 if __name__ == '__main__':
