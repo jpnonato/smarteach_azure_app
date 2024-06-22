dbe2c04f80bb38cf1ccb622ef922b9345396416a
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-22 03:15:58 -0300

    teste 123 11

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.get('/')
+def teste123():
+    return '<h1>---------IS WORKING 7777!!!--------- <h1>', 200
+
+
+if __name__ == '__main__':
+    app.run()
