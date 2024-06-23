b3410c29706950aa0f1b9449c516cb1db7c1419f
Joao Pedro Nonato Santos <jpnonato@yahoo.com.br>
2024-06-23 20:50:29 -0300

    add CORS policy logic

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
-
 from flask_app import init_app
+from flask_cors import CORS
 
 app = Flask(__name__)
 init_app(app)
+CORS(app)
 
 if __name__ == '__main__':
     app.run()
